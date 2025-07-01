0ebd4af6cb36522b50e76e0c6c48342a0c6c072f
hwain <immortal0900@gmail.com>
2025-07-01 19:29:13 +0900

    feat: Add function
    #1

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from typying import Union
+from typing import Union
 
 def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
     return x + y
 # [int, float] 는 정수나 실수가 출력될 수 있음을 의미함
 
-if __name_ == '__main__':
+if __name__ == '__main__':
     print(add(1, 2))
     print(add(1.5, 2.5))
     print(add(1, 2.5))
