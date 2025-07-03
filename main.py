d6f5ebab133aca8a83d50bc6c4f366d9d1ba1b3d
hwain <immortal0900@gmail.com>
2025-07-03 17:05:43 +0900

    refctor: add error handle

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from typing import Union
+from typing import Union, Optional
 
-def add(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
-    return x + y
+def add(x: Union[int, float], y: Union[int, float]) -> Optional[Union[int, float]]:
+    try:
+        result = x + y
+    except Exception as e:
+        print(e)
+        return None
+    
+    return result
 # [int, float] 는 정수나 실수가 출력될 수 있음을 의미함
 
 if __name__ == '__main__':
